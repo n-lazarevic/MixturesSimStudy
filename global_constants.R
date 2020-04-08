0abb1c7aeb8c9611bf509f496e1b160cc3b9696b
n-lazarevic <53067079+n-lazarevic@users.noreply.github.com>
2020-04-08 15:37:37 +1000

    Update global_constants.R
    Revision 1

--- global_constants.R
+++ global_constants.R
@@ -3,6 +3,7 @@
 #            estimating the non-linear health effects of correlated
 #            chemical mixtures: a simulation study."
 # Authors:   Nina Lazarevic, Luke D. Knibbs, Peter D. Sly, Adrian G. Barnett
+# Preprint:  https://arxiv.org/abs/1908.01583
 #
 # Written by Nina Lazarevic using R 3.4.3
 #
@@ -39,18 +40,18 @@ dgp.names<-c("loSNR.l","loSNR.s","loSNR.q","loSNR.a",
              "hiSNR.l","hiSNR.s","hiSNR.q","hiSNR.a")
 erfn.names <- c("linear", "loglogS", "quadinvU", "asymminvU")
 
-K <- 12 #total number of exposures
+K <- 17 #total number of exposures
 
 #outcome-associated exposures in each model
 K.true <- 4 #number of true exposures
 x.true.names <- c("MPB", "BP3", "PPB", "BPA")
 
 #constants by model (m1, m2)
-K.m1 <- 6 #number of exposures in model 1
-K.m2 <- 12 #number of exposures in model 2
-x.false.names.m1 <- c("MEP", "MHH")
-x.inc.m1 <- x.names %in% c(x.true.names,x.false.names.m1) #included in model 1
-x.inc.m2 <- rep(TRUE,K) #included exposures in model 2
+K.m1 <- 8 #number of exposures in model 1
+K.m2 <- 17 #number of exposures in model 2
+x.false.names.m1 <- c("MEP", "MOH", "MHH", "CNP")
+x.inc.m1 <- x.names %in% c(x.true.names, x.false.names.m1) #included in model 1
+x.inc.m2 <- rep(TRUE, K.m2) #included exposures in model 2
 x.names.m1 <- x.names[x.inc.m1] #names of exposures in model 1
 x.names.m2 <- x.names[x.inc.m2] #names of exposures in model 2
 scenario.names.m1 <- paste("m1", dgp.names, sep = ".")
@@ -78,11 +79,11 @@ b <- list(MPB = 2, BP3 = 2, PPB = 1, BPA = 1)
 
 #MCMC constants
 iter.burnin.bkmr <- 8000
-iter.bkmr <- 10000
+iter.bkmr <- 10000 #including burn-in
 iter.burnin.bart <- 4000
-iter.bart <- 6000
+iter.bart <- 6000 #including burn-in
 iter.burnin.star <- 8000
-iter.star <- 10000
+iter.star <- 10000 #including burn-in
 
 #random number generation seeds
 seed.sim_exposures <- 4936
